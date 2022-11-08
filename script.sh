ec0b5d6108ac0cd1b331f6e05f0587f3b1459e38
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2022-11-08 20:37:42 +0200

    update config to use env vars

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
-# first time
+## first time
+# set environment variables
+set DOMAIN_NAME=DOMAIN.TLD
+set LETSENCRYPT_EMAIL=email@example.com
+
+# run code
 git clone https://github.com/buzz-code/dockerized-server.git
 cd dockerized-server
-docker-compose up -d
+docker compose up -d
 
-# updating the script
+## updating the script
+git pull
+docker compose up -d
